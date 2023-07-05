dca42e2166075aed33accb97b935ac2a824f01dc
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-07-05 22:24:55 +0900

    ADD : 맛집 맵뷰 추가
    (cherry picked from commit 5c954e8f4949a81e7ae677287fb3a61087cc56b9)

---
+++ KuleumBridge/FoodStoreMapView.swift
@@ -0,0 +1,20 @@
+//
+//  FoodStoreMapView.swift
+//  KuleumBridge
+//
+//  Created by 최서연 on 2023/07/05.
+//
+
+import SwiftUI
+
+struct FoodStoreMapView: View{
+    var body: some View{
+        Text("맵뷰 띄우깅")
+    }
+}
+
+struct FoodStoreMapView_Previews: PreviewProvider{
+    static var previews: some View{
+        FoodStoreMapView()
+    }
+}
