91007282eb320b5ffefdbf3a9615f73d0dc70de4
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-07-05 10:07:14 +0900

    Initial Commit

---
+++ KuleumBridge/KuleumBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuleumBridgeApp.swift
+//  KuleumBridge
+//
+//  Created by 최서연 on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct KuleumBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
