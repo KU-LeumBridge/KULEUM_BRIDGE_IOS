c97480978951b5c5748728b8981b2dca56e1ca3c
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-07-06 22:59:35 +0900

    ADD : 공지사항 웹 버튼 구현
    (cherry picked from commit 1b241b5a98afc22f0c65edf8d4faa1f0148fc270)

---
+++ KuleumBridge/NoticeWebView.swift
@@ -0,0 +1,44 @@
+//
+//  NoticeWebView.swift
+//  KuleumBridge
+//
+//  Created by 최서연 on 2023/07/06.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct WKWebViewPractice: UIViewRepresentable {
+    var url: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: url) else {
+            return WKWebView()
+        }
+        let webView = WKWebView()
+
+        webView.load(URLRequest(url: url))
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: UIViewRepresentableContext<WKWebViewPractice>) {
+        guard let url = URL(string: url) else { return }
+        
+        webView.load(URLRequest(url: url))
+    }
+}
+
+// http 통신 허용 필요 - ATS의 Allow Arbitary Loads YES 설정
+struct NoticeWebView: View{
+    var body: some View{
+        WKWebViewPractice(url: "http://www.konkuk.ac.kr/jsp/Plaza/plaza_01_01.jsp")
+    }
+}
+
+struct NoticeWebView_Previews: PreviewProvider{
+    static var previews: some View{
+        WKWebViewPractice(url: "http://www.konkuk.ac.kr/jsp/Plaza/plaza_01_01.jsp")
+    }
+}
